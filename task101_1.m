3616c4c9e594632d033dfbe454eb9deb831efd02
wallbreaker01 <107265135+wallbreaker01@users.noreply.github.com>
2023-11-09 21:52:40 +0600

    Update task101_1.m

--- task101_1.m
+++ task101_1.m
@@ -1,4 +1,16 @@
 
-A=[1 2 3;4 5 6;7 8 9]
+function sowmik = task_101 ()
 
-a=max(max(A))
+   A = input("Matrix A : ");
+   B = input("Matrix B : ");
+   mx_A = max(max(A));
+   fprintf("Maximum value of A : %d\n", mx_A);
+   if size(A,2)==size(B,1)
+      Mul = A * B;
+      fprintf("Multiplication :\n");
+      disp (Mul);
+   else
+      fprintf ("Matrix multiplication is not possible!\n");
+   end
+
+endfunction
